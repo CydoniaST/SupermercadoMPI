873fa8f69e4c515740384ef43649a556898f598d
EricDev <145440108+CydoniaST@users.noreply.github.com>
2023-12-13 15:29:50 +0100

    Update Maestro-Esclavo.c
    Actualización en la que se eliminan las colas por arrays dinamicos.

--- Supermercado/Maestro-Esclavo.c
+++ Supermercado/Maestro-Esclavo.c
@@ -1,123 +1,151 @@
-#include <stdio.h>
+// Practica2ASO.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
 #include <mpi.h>
+#include <windows.h>
 #include <stdlib.h>
-#include <time.h>
+#include<time.h>
 
 
-#define MAESTRO 0
-#define CLIENTE_PRIORITARIO 1
-#define CLIENTE_NORMAL 0
-#define CAJA_PRIORITARIA 1
-#define CAJA_NORMAL 0
-
-//Sistema de cola de clientes
-typedef struct{
-    int id;
-    int prioridad;
-}Cliente;
-
-
-typedef struct{
-    Cliente cliente;
-    struct cola *siguiente;
-} Nodo;
-
-
-typedef struct{
-    Nodo* inicioCola;
-    Nodo* finalCola;
-} Cola;
-//Final de Sistema de cola de clientes
-
-
-void supermercado(){//Este metodo debe crear las colas, asignar clientes.
-
-    MPI_Status status;
-
-    int cliente;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    if(rank == MAESTRO){
-        cajasAbiertas = size/2;
-
-        while(clienteID < MAX_CLIENTES){
-            
-        }
-    }else{
-
-        while(1){
-            MPI_Recv( &cliente , MPI_INT ,0 , MPI_ANY_TAG , MPI_Comm comm , &status);
-        }
-    }
-}
-
-Cola* iniciarCola(){//Este metodo inicializa una cola vacia
-    Cola* nuevaCola = (Cola*)malloc(sizeof(Cola));
-    if(nuevaCola==NULL){
-        printf("Error: no se ha podido crear la cola.");
-        return -1;
-    }
-    cola->inicioCola = NULL;
-    cola->finalCola = NULL;
-    return nuevaCola;
-}
-
-void nuevoCliente(Cola* cola, Cliente cliente){//Este emetodo añade un nuevo cliente a la cola de cliente segun llegan
-    Nodo* nuevoNodo = (Nodo*)malloc(sizeof(Nodo));
-    nuevoNodo -> cliente = cliente;
-    nuevoNodo-> siguiente = NULL;
-
-    if(cola->finalCola == NULL){
-        cola->inicioCola = nuevoNodo;
-        cola->finalCola = nuevoNodo;
-    }else{
-        cola->finalCola->siguiente = nuevoNodo;
-        cola->finalCola = nuevoNodo;
-    }
-}
-
-void saleCliente(Cola* cola){//Este metodo elimina un cliente de la cola
-    if(cola->inicioCola == NULL){
-        printf("La cola esta vacia, no puede salir ningun cliente");
-    }
+//Variables Necesarias
+#define MAX_CLIENTES 100
+void clienteLlega() {
     
-    Nodo* nodoEliminado = cola->inicioCola;
-    cola->inicioCola = cola->inicioCola->siguiente;
-    free(nodoEliminado);
-    if(cola->inicioCola == NULL){
-        Cliente clienteVacio = {-1, -1};
-        cola->finalCola = NULL;
+
+   
+}
+
+void clienteAtendido(int cliente) {
+    printf("El cliente %d va a ser atendido.\n", cliente);
+    fflush(stdout);
+
+    srand(time(NULL));
+    int num = 1 + rand() % (6 - 1);
+
+    Sleep(num);
+
+    printf("El cliente %d ha sido atendido.\n", cliente);
+    fflush(stdout);
+}
+
+void gestionDeClientes(int pid, int np, int* clientes, int numClientes) {
+
+   
+    if (pid == 0) {//Proceso Maestro (Clientes)
+        int cajaDestino = 1;
+        int clientesNuevos;
+        int trabajo = 1;
+        
+      
+        for (int i = 0; i < numClientes; i++) {//Bucle de envios de mensaje a los exclaves apra indicar si hay clientes o no en la cola
+
+            if (i >= numClientes - (np - 1)) { //Si en la cola no hay clientes el trabajo pasa a ser 0 y se lo comunicamos a las cajas (np)
+                trabajo = 0;
+                MPI_Send(&trabajo, 1, MPI_INT, cajaDestino, 0, MPI_COMM_WORLD);
+            }
+            else {//Si en la cola aun hay clientes se lo comunicamos a las cajas para que sigan trabajando
+                MPI_Send(&trabajo, 1, MPI_INT, cajaDestino, 0, MPI_COMM_WORLD);
+            }
+            
+            MPI_Send(&clientes[i], 1, MPI_INT, cajaDestino, 0, MPI_COMM_WORLD);
+
+            printf("\nEnviado cliente %d a la caja correspondiente.", clientes[i]);
+            fflush(stdout);
+
+            cajaDestino = (cajaDestino % np) + 1;
+
+            if (cajaDestino == np) {//Cuando el destino sea igual al numero de procesos debera volver al nodo 1 y reiniciar los envios.
+                cajaDestino = 1;
+       		}	
+
+            //MPI_Recv()
+        }
+
     }
+    else {//Procesos Esclavos (Cajas)
 
-   free(nodoEliminado);
-}
-void comprobarHuecoCaja (){ //Metodo que comprueba si hau una caja libre para que entre un cliente
+        int trabajoEsclavo = 1;
+        while (trabajoEsclavo) {
+            int clienteRecibido;
+            MPI_Recv(&trabajoEsclavo, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&clienteRecibido, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Soy la caja %d y me ha llegado el cliente %d\n", pid, clienteRecibido);
+            fflush(stdout);
+            clienteAtendido(clienteRecibido);
 
-}
+            MPI_Send(&clienteRecibido, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
 
-void clienteLlega(Cola* cola, int idCliente, int prioridadCliente){//LLega un cliente nuevo y lo mete en la cola
-    Cliente nuevoCliente = { idCliente, prioridadCliente};
-    nuevoCliente(cola, nuevoCliente);
-    printf("El clientecon ID %d ha sido atendido y tiene prioridad %d\n", nuevoCliente.id, nuevoCliente.prioridad)
-}
-
-void clienteAtendido(Cola* cola){//Atiende un cliente en la caja y lo elimina de la cola
-    Cliente clienteAtendido = saleCliente(cola);
-
-    if(clienteAtendido.id != -1){
-        printf("El cliente con ID: %d ha sido atendido", clienteAtendido.id);
-    }else{
-        printf("No hay cliente para atender.");
     }
+    if (pid == 0) {
+        free(clientes);
+    }
+         
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
+    int pid, np;
 
-    int np, size;
+    MPI_Init(NULL, NULL); //AQUI INICIA MPI Y DIVIDE TODO ENTRE LOS PROCESOS PARA EJECUTAR LA PARTE DE MPI
 
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+   
+    double inicio, fin, tiempo;
+    //Metodo encargado de gestionar la entrada de los clientes en las cajas
+    //Encargado de seguir esquema Maestro-Esclavo
+    //Primero craeamos la cola de clientes
+    
+    int j = 0;
+    int* clientes = NULL;
+    int numClientes = atoi(argv[1]);
 
-    MPI_Init(&argc, &argv);   
+    //Establecemos las cajas abiertas segun el numero de clientes
+    int* cajas;
+    int numCajas = numClientes * 2, x = 0;
+
+    if (pid == 0) {//El proceso MAESTRO Inicia la cola y la imprime
+        printf("El size de la cola es: %d. ", numClientes);
+
+        clientes = (int*)malloc(sizeof(int) * numClientes);
+
+        if (clientes == NULL) {
+            printf("ERROR: No se pudo reservar memoria.");
+            exit(1);
+        }
+
+        for (int i = 0; i < numClientes; i++) {
+            clientes[i] = j + 1;
+            j++;
+        }
+
+        printf("\nLos clientes de la cola son los siguientes: ");
+        printf("[");
+        for (int i = 0; i < numClientes; i++) {
+            printf(" %d", clientes[i]);
+        }
+        printf("]");
+        //Creacion de cola finalizada
+
+        //Copia de Clientes para que no se sobreescriba (por seguridad)
+         //int* bufferClientes = (int*) malloc(sizeof(int));
+
+        cajas = (int*)malloc(sizeof(int) * numCajas);
+
+        for (int i = 0; i < numCajas; i++) {
+            cajas[i] = x + 1;
+            x++;
+        }
+
+        printf("\nIniciando trabajo del supermercado.");
+        fflush(stdout);     
+    }
+    gestionDeClientes(pid, np, clientes, numClientes);
+
+    MPI_Finalize();
+    return 0;
 }
+
+
